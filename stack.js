f436f87400a7c7b08ba01353b8c33b0d25dbf75c
查子红 <chazihong@szewec.com>
2021-07-29 15:39:05 +0800

    使用栈实现二叉树的前序遍历

---
+++ stack.js
@@ -0,0 +1,30 @@
+/**
+ * 二叉树的前序遍历 ：使用栈通过迭代算法实现
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+ */
+// /**
+//  * Definition for a binary tree node.
+//  * function TreeNode(val, left, right) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.left = (left===undefined ? null : left)
+//  *     this.right = (right===undefined ? null : right)
+//  * }
+//  */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+    let res = [];
+    let stack = [];
+    if (root) stack.push(root);
+    while (stack.length) {
+        const n = stack.pop();
+        res.push(n.val);
+        if (n.right) stack.push(n.right);
+        if (n.left) stack.push(n.left);
+    }
+    return res;
+};
